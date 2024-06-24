d694607b62c8ef43f93256a88597ee51fc785965
Abhishek kumar <abhishek08310@gmail.com>
2024-06-24 10:52:13 +0530

    abhi

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './src/index.css';
+import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "remixicon/fonts/remixicon.css";
